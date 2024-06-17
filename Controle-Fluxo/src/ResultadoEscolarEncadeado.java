1e030be6007edc1f6a2d1a2d5786d49a6b161917
Robert Fernandes <robertfde.souza@gmail.com>
2024-06-17 20:40:41 -0300

    Condicional Encadeada

---
+++ Controle-Fluxo/src/ResultadoEscolarEncadeado.java
@@ -0,0 +1,15 @@
+public class ResultadoEscolarEncadeado {
+    // Estrutura Condicional Encadeada
+    public static void main(String[] args) {
+        int nota = 7;
+
+        if (nota >= 7)
+            System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7);
+            System.out.println("Recuperação");
+
+        else
+            System.out.println("Reprovado");
+    }
+}
