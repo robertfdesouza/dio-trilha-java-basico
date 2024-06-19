549cb552adf0fd796fd280d45c07aa048fc25c1b
Robert Fernandes <robertfde.souza@gmail.com>
2024-06-19 20:58:27 -0300

    Ternario 2

--- Controle-Fluxo/src/ResultadoEscolarTernario.java
+++ Controle-Fluxo/src/ResultadoEscolarTernario.java
@@ -1,7 +1,7 @@
 public class ResultadoEscolarTernario {
     public static void main(String[] args) {
-        int nota = 9;
-        String Resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        int nota = 4;
+        String Resultado = nota >=7 ? "Aprovado" : nota >=5 && nota < 7 ? "Recuperação" : "Reprovado";
        
         System.out.println(Resultado);
     }
