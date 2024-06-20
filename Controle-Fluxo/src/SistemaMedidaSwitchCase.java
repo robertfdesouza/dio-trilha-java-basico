351d9b4870fb44a0d6ae1e272cfb5838377ef7df
Robert Fernandes <robertfde.souza@gmail.com>
2024-06-19 21:06:09 -0300

    Swtch Case

---
+++ Controle-Fluxo/src/SistemaMedidaSwitchCase.java
@@ -0,0 +1,14 @@
+public class SistemaMedidaSwitchCase {
+    public static void main(String[] args) {
+        String sigla = "M";
+
+        if (sigla == "P");
+            System.out.println("Pequeno");
+        else if (sigla == "M");
+            System.out.println("MEDIA");
+        else if (sigla == "G")
+            System.out.println("Grande");
+        else
+            System.out.println("Indefinido");
+    }
+}
