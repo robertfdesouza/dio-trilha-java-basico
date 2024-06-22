2307b6d3461e5697cba5c4eecf993bb3c154c092
Robert Fernandes <robertfde.souza@gmail.com>
2024-06-22 09:45:08 -0300

    ExemploBreakContinueJava

--- Controle-Fluxo/src/ExemploForArray.java
+++ Controle-Fluxo/src/ExemploForArray.java
@@ -1,10 +1,10 @@
 public class ExemploForArray {
     public static void main(String[] args) {
 
-        String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
-
+        String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS", "ROBERT", "ISABELLA"};
+        // INDICE DE ARRAY SEMPRE COMEÇA COM ZERO
         for (int x = 0; x<alunos.length; x++){
-            System.out.println("O aluno no indice x= " + x + " é " + alunos[x]);
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
         }
         
     }
