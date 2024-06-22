a9518b46ad3e0226bfabd706de181e395d5f4a1b
Robert Fernandes <robertfde.souza@gmail.com>
2024-06-22 09:50:21 -0300

    Alterações 2

--- Controle-Fluxo/src/ExemploBreakContinue.java
+++ Controle-Fluxo/src/ExemploBreakContinue.java
@@ -2,7 +2,8 @@ public class ExemploBreakContinue {
     public static void main(String[] args) {
         for (int numero = 1; numero <= 5; numero ++){
             if(numero == 3)
-            break;
+            continue; // o continuie não para a interação somente não imprime o número 03
+                      // Já o break interrompe a interação no número 03 imprimindo somente os números 1 e 2.
 
             System.out.println(numero);
         }
